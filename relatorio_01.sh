1fcf665209beeef6fc61ca6c21f8e39ba04990f2
EdmarSantosM <santoem4@hotmail.com>
2023-12-17 21:49:47 -0300

    Update relatorio_01.sh

--- relatorio_01.sh
+++ relatorio_01.sh
@@ -21,13 +21,8 @@ echo ""
 echo -e " Memória (\e[1;34m Utilização / Livre) \e[0m"
 echo " -------------------------------------------------------------------------------- "
 free -h  # Comando no Linux 
-mem
+# mem
 echo " -------------------------------------------------------------------------------- "
-
-# echo ""
-# echo " Teste no  Windows"
-# systeminfo
-# echo ""
 
 echo ""
 echo -e " Discos (\e[1;34m SDA / Partiçoes \e[0m)"
